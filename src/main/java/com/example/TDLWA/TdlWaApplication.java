aba590192194cc46ebc2ff282a04f219f45d326c
Your Name <you@example.com>
2021-03-11 10:58:58 +0000

    updated to .war

---
+++ src/main/java/com/example/TDLWA/TdlWaApplication.java
@@ -0,0 +1,13 @@
+package com.example.TDLWA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdlWaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdlWaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/qa/tdl/TDLWA/TdlWaApplication.java
@@ -0,0 +1,13 @@
+package com.qa.tdl.TDLWA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdlWaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdlWaApplication.class, args);
+	}
+
+}
